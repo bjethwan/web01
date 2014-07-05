60658a199cc4a3f987cc60e89df81aabf97b28ce
Bipin Jethwani <jethwani.bipin@gmail.com>
2014-07-06 00:02:57 +0530

    initial commit

---
+++ src/main/java/com/bjethwan/Test01.java
@@ -0,0 +1,20 @@
+package com.bjethwan;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Test01 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h3>Hello ......I told you already that jenkins rocks.....Bye!!!</h3>");
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! - Jenkins Rocks</h2>
+</body>
+</html>
